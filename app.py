9887a3405969deeaf0eebf1decc0449183e8759e
Saksham Jain <ersakshamjain@gmail.com>
2017-03-15 19:58:07 +0530

    phone no and email added

--- app.py
+++ app.py
@@ -21,102 +21,105 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+	req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+	print("Request:")
+	print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+	res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+	res = json.dumps(res, indent=4)
+	# print(res)
+	r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
 
 
 def processRequest(req):
 	with open('result.json') as data_file:
 		data = json.load(data_file)
-	if req["result"]["metadata"]["intentName"]=="name":
+	intentName=req["result"]["metadata"]["intentName"] #to get the name of intent used so that appropriate answer can be given
+	if intentName=="name":
 		result=data["name"]
-	elif req["result"]["metadata"]["intentName"]=="Age":
-		result=data["age"]
+	elif intentName=="phoneno":
+		result=data["phoneno"]
+	elif intentName=="email":
+		result=data["email"] #fetch from json file the corresponding keyword string
 		#data=json.loads(result)
-	res=makeWebhookResult(result,req["result"]["metadata"]["intentName"])
+	res=makeWebhookResult(result,intentName)
 	return res
 
 
-    #if req.get("result").get("action") != "yahooWeatherForecast":
-     #   return {}
-    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-     #   return {}
-    #yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    #result = urlopen(yql_url).read()
-    #data = json.loads(result)
-    #res = makeWebhookResult(data)
-    #return res
+	#if req.get("result").get("action") != "yahooWeatherForecast":
+	 #   return {}
+	#baseurl = "https://query.yahooapis.com/v1/public/yql?"
+	#yql_query = makeYqlQuery(req)
+	#if yql_query is None:
+	 #   return {}
+	#yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+	#result = urlopen(yql_url).read()
+	#data = json.loads(result)
+	#res = makeWebhookResult(data)
+	#return res
 
 def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+	result = req.get("result")
+	parameters = result.get("parameters")
+	city = parameters.get("geo-city")
+	if city is None:
+		return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+	return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data,intent):
-    #query = data.get('query')
-    #if query is None:
-     #   return {}
+	#query = data.get('query')
+	#if query is None:
+	 #   return {}
 
-    #result = query.get('results')
-    #if result is None:
-     #   return {}
+	#result = query.get('results')
+	#if result is None:
+	 #   return {}
 
-    #channel = result.get('channel')
-    #if channel is None:
-     #   return {}
+	#channel = result.get('channel')
+	#if channel is None:
+	 #   return {}
 
-    #item = channel.get('item')
-    #location = channel.get('location')
-    #units = channel.get('units')
-    #if (location is None) or (item is None) or (units is None):
-     #   return {}
+	#item = channel.get('item')
+	#location = channel.get('location')
+	#units = channel.get('units')
+	#if (location is None) or (item is None) or (units is None):
+	 #   return {}
 
-    #condition = item.get('condition')
-    #if condition is None:
-     #   return {}
+	#condition = item.get('condition')
+	#if condition is None:
+	 #   return {}
 
-    # print(json.dumps(item, indent=4))
+	# print(json.dumps(item, indent=4))
 
-    #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-     #        ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-    # speech= "My name is" + data +",Nice to meet you!"
+	#speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+	 #        ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+	# speech= "My name is" + data +",Nice to meet you!"
 	#speech=""
 	if intent == 'name':
 		speech="My name is "+data+ ", Nice to meet you!"
-	elif intent == 'Age':
-		speech=speech+"I am "+data+" years old!"
+	elif intent=='phoneno':
+		speech="You can give me a call at "+data
 	print("Response:")
 	print(speech)
 	return {
 	"speech": speech,
 	"displayText": speech,
-        # "data": data,
-        # "contextOut": [],
+		# "data": data,
+		# "contextOut": [],
 	"source": "SmartResume"
-    }
+	}
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+	port = int(os.getenv('PORT', 5000))
 
-    print("Starting app on port %d" % port)
+	print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+	app.run(debug=False, port=port, host='0.0.0.0')
 
